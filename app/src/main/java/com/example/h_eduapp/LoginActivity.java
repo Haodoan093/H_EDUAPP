c75c90eef42e585e13e00b9b67c2e9df8040d90f
Haodoan093 <122719130+Haodoan093@users.noreply.github.com>
2024-01-23 00:12:55 +0700

    SignIn With GG

--- app/src/main/java/com/example/h_eduapp/LoginActivity.java
+++ app/src/main/java/com/example/h_eduapp/LoginActivity.java
@@ -9,6 +9,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -17,14 +18,24 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 100;
+    GoogleSignInClient mGoogleSignInClient;
     EditText mEmailEt, mPassswordEt;
     Button mLoginbtn;
 
@@ -35,6 +46,10 @@ public class LoginActivity extends AppCompatActivity {
 
     //Declare an instance of FirebaseAuth
     private FirebaseAuth mAuth;
+
+
+    //Login Google
+    SignInButton mGoogleLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,14 +62,18 @@ public class LoginActivity extends AppCompatActivity {
         mLoginbtn = findViewById(R.id.loginBtn);
         mNotHaveAccount = findViewById(R.id.nothave_accountTv);
         mRecoverPassTv = findViewById(R.id.recoverPassTv);
+        mGoogleLoginBtn = findViewById(R.id.googleLoginBtn);
+
+        //before mAuth
+        //Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.app_name)).requestEmail().build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
 
         ///in the onCreate() method, initalize the FirebaseAuth instance.
         mAuth = FirebaseAuth.getInstance();
-
-        //init progress dialog
-        progressDialog = new ProgressDialog(this);
-
 
         mLoginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,6 +108,21 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
+        //handle google login btn click
+        mGoogleLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+
+            }
+        });
+
+
+        //init progress dialog
+        progressDialog = new ProgressDialog(this);
+
+
     }
 
     private void showRecoverPasswordDialoog() {
@@ -103,7 +137,8 @@ public class LoginActivity extends AppCompatActivity {
         final EditText emailEt = new EditText(this);
         emailEt.setHint("Email");
         emailEt.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
-
+//    sets the min width of a EditView to fit a text of n 'M ' letters regardle
+//                extension and text size
         emailEt.setMinEms(16);
 
 
@@ -118,7 +153,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialogInterface, int i) {
                 //input email
                 String email = emailEt.getText().toString().trim();
-               beginRecovery(email);
+                beginRecovery(email);
             }
         });
         //buttons cancel
@@ -132,10 +167,11 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
-    ///show dialog
+        ///show dialog
         builder.show();
 
     }
+
     private void beginRecovery(String email) {
         progressDialog.setMessage("Sending email...");
 
@@ -145,14 +181,12 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         progressDialog.dismiss();
-                        if(task.isSuccessful()) {
-                            Toast.makeText(LoginActivity.this,"Email sent",Toast.LENGTH_SHORT).show();
-                        }
-                        else{
-                            Toast.makeText(LoginActivity.this,"Failed...",Toast.LENGTH_SHORT).show();
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Email sent", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Failed...", Toast.LENGTH_SHORT).show();
                         }
 
- 
 
                     }
                 }).addOnFailureListener(new OnFailureListener() {
@@ -160,7 +194,7 @@ public class LoginActivity extends AppCompatActivity {
                     public void onFailure(@NonNull Exception e) {
                         progressDialog.dismiss();
                         //get and show proper error messagge
-                        Toast.makeText(LoginActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                        Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
 
                     }
                 });
@@ -206,4 +240,51 @@ public class LoginActivity extends AppCompatActivity {
         onBackPressed();
         return super.onSupportNavigateUp();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                Toast.makeText(this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct){
+
+        AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(firebaseCredential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this,""+user.getEmail(),Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
+                            finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(LoginActivity.this,"Login Failed",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
 }
