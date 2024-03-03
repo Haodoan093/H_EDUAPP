6cc53c4f5b3d0d7551c25850e827d0fb58c7523e
Haodoan093 <122719130+Haodoan093@users.noreply.github.com>
2024-03-03 13:46:38 +0700

    Show Registered Users

---
+++ app/src/main/java/com/example/h_eduapp/ModelUsers.java
@@ -0,0 +1,88 @@
+package com.example.h_eduapp;
+
+public class ModelUsers {
+
+    // Use the same names as in the Firebase database
+    String name, email, search, phone, image, cover,uid;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public ModelUsers(String name, String email, String search, String phone, String image, String cover, String uid) {
+        this.name = name;
+        this.email = email;
+        this.search = search;
+        this.phone = phone;
+        this.image = image;
+        this.cover = cover;
+        this.uid = uid;
+    }
+
+    // Empty constructor (required by Firebase)
+    public ModelUsers() {
+    }
+
+    // Constructor with parameters
+    public ModelUsers(String name, String email, String search, String phone, String image, String cover) {
+        this.name = name;
+        this.email = email;
+        this.search = search;
+        this.phone = phone;
+        this.image = image;
+        this.cover = cover;
+    }
+
+    // Getter and setter methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+}
--- app/src/main/java/com/example/h_eduapp/UserFragment.java
+++ app/src/main/java/com/example/h_eduapp/UserFragment.java
@@ -1,64 +1,84 @@
 package com.example.h_eduapp;
-
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link UserFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class UserFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    RecyclerView recyclerView;
+    AdapterUsers  adapterUsers;
+    List<ModelUsers> usersList;
     public UserFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UserFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UserFragment newInstance(String param1, String param2) {
-        UserFragment fragment = new UserFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
+
+        View view= inflater.inflate(R.layout.fragment_user, container, false);
+
+
+        recyclerView= view.findViewById(R.id.users_recyclerView);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+// init user list
+        usersList= new ArrayList<>();
+        getAllUsers();
+        return view;
+
+    }
+
+    private void getAllUsers() {
+        FirebaseUser firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+        //get path og datatbase named "Users" containing users info
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference("Users");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                usersList.clear();
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    ModelUsers  modelUsers= ds.getValue(ModelUsers.class);
+
+                    //get all
+                    if(!modelUsers.getUid().equals(firebaseUser.getUid())){
+                        usersList.add(modelUsers);
+                    }
+
+                    adapterUsers = new AdapterUsers(getActivity(),usersList);
+
+                    recyclerView.setAdapter(adapterUsers);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
     }
 }
