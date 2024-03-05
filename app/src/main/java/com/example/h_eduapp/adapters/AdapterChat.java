0aa0bf72b08b5031d2d8d09c821cb73d69359863
Haodoan093 <122719130+Haodoan093@users.noreply.github.com>
2024-03-05 21:02:17 +0700

    Delete Message

--- app/src/main/java/com/example/h_eduapp/adapters/AdapterChat.java
+++ app/src/main/java/com/example/h_eduapp/adapters/AdapterChat.java
@@ -1,11 +1,16 @@
 package com.example.h_eduapp.adapters;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,6 +19,12 @@ import com.example.h_eduapp.UserFragment;
 import com.example.h_eduapp.models.ModelChat;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
@@ -21,18 +32,20 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
-public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder>{
+public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder> {
 
- private static final int MSG_TYPE_LEFT=0;
- private static final int MSG_TYPE_RIGHT=1;
- Context context ;
- List<ModelChat> chatList;
- String imageUrl;
+    private static final int MSG_TYPE_LEFT = 0;
+    private static final int MSG_TYPE_RIGHT = 1;
+    Context context;
+    List<ModelChat> chatList;
+    String imageUrl;
 
- FirebaseUser fUser;
+
+    FirebaseUser fUser;
 
     public AdapterChat(Context context, List<ModelChat> chatList, String imageUrl) {
         this.context = context;
@@ -44,18 +57,18 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder>{
     @Override
     public MyHolder onCreateViewHolder(@androidx.annotation.NonNull ViewGroup parent, int viewType) {
         //inflate ;ayouts : row_chatleft ot right
-        if (viewType==MSG_TYPE_RIGHT){
-            View view = LayoutInflater.from(context).inflate(R.layout.row_chat_right,parent,false);
+        if (viewType == MSG_TYPE_RIGHT) {
+            View view = LayoutInflater.from(context).inflate(R.layout.row_chat_right, parent, false);
             return new MyHolder(view);
-        }else {
-            View view = LayoutInflater.from(context).inflate(R.layout.row_chat_left,parent,false);
+        } else {
+            View view = LayoutInflater.from(context).inflate(R.layout.row_chat_left, parent, false);
             return new MyHolder(view);
         }
 
     }
 
     @Override
-    public void onBindViewHolder(@androidx.annotation.NonNull MyHolder holder, int position) {
+    public void onBindViewHolder(@androidx.annotation.NonNull MyHolder holder, @SuppressLint("RecyclerView") final int position) {
         //get data
         String message = chatList.get(position).getMessage();
         String timeStamp = chatList.get(position).getTimestamp();
@@ -85,6 +98,37 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder>{
             // Handle exception if Picasso fails to load image
         }
 
+        //click to show delete dialog
+
+        holder.messageLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Choose Action");
+                // Add the buttons
+                builder.setPositiveButton("Recall", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        recallMessage(position);
+                    }
+                });
+                builder.setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        deleteMessage(position);
+                    }
+                });
+                builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                        dialog.dismiss();
+                    }
+                });
+                // Create and show the AlertDialog
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+
         //set seen/delivered status of message
         if (position == chatList.size() - 1) {
             if (chatList.get(position).getSeen() != null && chatList.get(position).getSeen()) {
@@ -97,6 +141,65 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder>{
         }
     }
 
+    private void deleteMessage(int position) {
+
+        String myUid= FirebaseAuth.getInstance().getUid();
+        String msgTimeStamp= chatList.get(position).getTimestamp();
+        DatabaseReference dbRef= FirebaseDatabase.getInstance().getReference("Chats");
+        Query query=dbRef.orderByChild("timestamp").equalTo(msgTimeStamp);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@androidx.annotation.NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds:snapshot.getChildren()){
+
+                    if(ds.child("sender").getValue().equals(myUid)){
+                        //xoa
+                        ds.getRef().removeValue();
+
+                    }else{
+                        Toast.makeText(context, "You can delete only your messages...", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@androidx.annotation.NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    private void recallMessage(int position) {
+
+        String myUid= FirebaseAuth.getInstance().getUid();
+        String msgTimeStamp= chatList.get(position).getTimestamp();
+        DatabaseReference dbRef= FirebaseDatabase.getInstance().getReference("Chats");
+        Query query=dbRef.orderByChild("timestamp").equalTo(msgTimeStamp);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@androidx.annotation.NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds:snapshot.getChildren()){
+
+                    if(ds.child("sender").getValue().equals(myUid)){
+
+                        //thu hooi
+                        HashMap<String,Object> hashMap= new HashMap<>();
+                        hashMap.put("message","This message was deleted...");
+                        ds.getRef().updateChildren(hashMap);
+                        Toast.makeText(context, "message deleted...", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(context, "You can delete only your messages...", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@androidx.annotation.NonNull DatabaseError error) {
+
+            }
+        });
+    }
 
 
     @Override
@@ -107,10 +210,10 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder>{
     @Override
     public int getItemViewType(int position) {
         //get currently signed in user
-        fUser= FirebaseAuth.getInstance().getCurrentUser();
-        if(chatList.get(position).getSender().equals(fUser.getUid())){
+        fUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (chatList.get(position).getSender().equals(fUser.getUid())) {
             return MSG_TYPE_RIGHT;
-        }else{
+        } else {
             return MSG_TYPE_LEFT;
         }
 
@@ -121,7 +224,8 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder>{
 
 
         ImageView mProfileIv;
-        TextView timeTv,messageTv, mSeenTv;
+        TextView timeTv, messageTv, mSeenTv;
+        LinearLayout messageLayout;
 
         public MyHolder(@NonNull View view) {
 
@@ -131,6 +235,8 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder>{
             timeTv = view.findViewById(R.id.timeTv);
             mSeenTv = view.findViewById(R.id.isSeenTv);
             mProfileIv = view.findViewById(R.id.profileIv);
+
+            messageLayout = view.findViewById(R.id.messageLayout);
 
         }
 
