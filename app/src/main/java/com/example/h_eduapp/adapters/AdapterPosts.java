2930db73aa175911e7d8438395eb0a01cff992c5
Haodoan093 <122719130+Haodoan093@users.noreply.github.com>
2024-03-19 07:02:34 +0700

    Delete Posts

--- app/src/main/java/com/example/h_eduapp/adapters/AdapterPosts.java
+++ app/src/main/java/com/example/h_eduapp/adapters/AdapterPosts.java
@@ -1,14 +1,19 @@
 package com.example.h_eduapp.adapters;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +23,16 @@ import com.example.h_eduapp.R;
 import com.example.h_eduapp.ThereProfileActivity;
 import com.example.h_eduapp.models.ModelChat;
 import com.example.h_eduapp.models.ModelPoost;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
@@ -32,9 +47,12 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
     Context context;
     List<ModelPoost> postList;
 
+    String myUid;
+
     public AdapterPosts(Context context, List<ModelPoost> postList) {
         this.context = context;
         this.postList = postList;
+        myUid= FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 
 
@@ -90,6 +108,7 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
                         holder.pImageIv.setVisibility(View.GONE);
         }else
         {
+            holder.pImageIv.setVisibility(View.VISIBLE);
             try {
                 Picasso.get().load(pImage).into(holder.pImageIv);
             } catch (Exception e) {
@@ -104,7 +123,7 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
         holder.moreBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(context, "More", Toast.LENGTH_SHORT).show();
+               showMoreOptions(holder.moreBtn,uid,myUid,pId,pImage);
             }
         });
         //like
@@ -142,6 +161,112 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
 
 
     }
+
+    private void showMoreOptions(ImageButton moreBtn, String uid, String myUid, String pId, String pImage) {
+        PopupMenu popupMenu = new PopupMenu(context,moreBtn, Gravity.END);
+
+
+
+        if(uid.equals(myUid)){
+            popupMenu.getMenu().add(Menu.NONE,0,0,"Delete");
+        }
+
+
+        //item click listener
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                int id= menuItem.getItemId();
+                if(id==0){
+                    //delete is clicked
+                    beginDelte(pId,pImage);
+
+                }
+                return false;
+            }
+        });
+
+        popupMenu.show();
+    }
+
+    private void beginDelte(String pId, String pImage) {
+
+        if(pImage.equals("noImage")){
+            deleteWithoutImage(pId);
+        }else{
+            deleteWithImage(pId,pImage);
+        }
+    }
+
+    private void deleteWithoutImage(String pId) {
+        ProgressDialog pd= new ProgressDialog(context);
+        pd.setMessage("Deleting...");
+        Query query= FirebaseDatabase.getInstance().getReference("Posts").orderByChild("pId").equalTo(pId);
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@androidx.annotation.NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds: snapshot.getChildren()){
+                    ds.getRef().removeValue();//rremove values from firebbase where pId matches
+
+                }
+                Toast.makeText(context, "Deleted successfully", Toast.LENGTH_SHORT).show();
+                pd.dismiss();
+
+            }
+
+            @Override
+            public void onCancelled(@androidx.annotation.NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void deleteWithImage(String pId, String pImage) {
+
+        ProgressDialog pd= new ProgressDialog(context);
+        pd.setMessage("Deleting...");
+//del image using url
+        //del froom data using post id
+        StorageReference picRef= FirebaseStorage.getInstance().getReferenceFromUrl(pImage);
+        picRef.delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+
+                        Query query= FirebaseDatabase.getInstance().getReference("Posts").orderByChild("pId").equalTo(pId);
+
+                        query.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@androidx.annotation.NonNull DataSnapshot snapshot) {
+                                for(DataSnapshot ds: snapshot.getChildren()){
+                                    ds.getRef().removeValue();//rremove values from firebbase where pId matches
+                                    
+                                }
+                                Toast.makeText(context, "Deleted successfully", Toast.LENGTH_SHORT).show();
+                                pd.dismiss();
+
+                            }
+
+                            @Override
+                            public void onCancelled(@androidx.annotation.NonNull DatabaseError error) {
+
+                            }
+                        });
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@androidx.annotation.NonNull Exception e) {
+                        pd.dismiss();
+                        Toast.makeText(context, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
 
     @Override
     public int getItemCount() {
